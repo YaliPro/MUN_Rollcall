765f44aee93bb2e71cda46ce20e9179d2166ee3a
Quentin <Quentin_L_0415@outlook.com>
2016-09-06 14:06:04 +0800

    add DelegateRepositoryImp

---
+++ src/main/java/model/repository/DelegateRepositoryImp.java
@@ -0,0 +1,7 @@
+package model.repository;
+
+/**
+ * Created by Quentin on 9/6/16.
+ */
+public class DelegateRepositoryImp {
+}
---
+++ src/test/java/model/repository/DelegateRepositoryImpTest.java
@@ -0,0 +1,8 @@
+import static org.junit.Assert.*;
+
+/**
+ * Created by Quentin on 9/6/16.
+ */
+public class DelegateRepositoryImpTest {
+
+}
